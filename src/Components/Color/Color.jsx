03be86846b0c14e28f2a4dcab2abd4ea34c1a51f
Ole Backhaus <ole.backhaus@gmx.de>
2024-08-05 12:52:58 +0200

    Add ColorPicker Tool part #1

--- src/Components/Color/Color.jsx
+++ src/Components/Color/Color.jsx
@@ -7,8 +7,7 @@ export default function Color({ color }) {
       style={{
         background: color.hex,
         color: color.contrastText,
-      }}
-    >
+      }}>
       <h3 className="color-card-headline">{color.hex}</h3>
       <h4>{color.role}</h4>
       <p>contrast: {color.contrastText}</p>
