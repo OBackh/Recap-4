3ee45abec7dd5c4a67f0a3c8c9c44a62e67aef41
Ole Backhaus <ole.backhaus@gmx.de>
2024-08-05 16:19:55 +0200

    Add "New Color to the Theme" Funktion

--- src/Components/ColorInput/ColorInput.jsx
+++ src/Components/ColorInput/ColorInput.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-export default function ColorInput({ id, defaultValue }) {
+export default function ColorInput({ id, name, defaultValue }) {
   const [inputValue, setInputValue] = useState(defaultValue);
 
   function handleInputValue(event) {
@@ -10,7 +10,7 @@ export default function ColorInput({ id, defaultValue }) {
       <input
         type="text"
         id={id}
-        name={id}
+        name={name}
         value={inputValue}
         onChange={handleInputValue}
       />
