d3c27d1ecc45dda96145916b8a194cc446e87762
Ole Backhaus <ole.backhaus@gmx.de>
2024-08-06 10:45:40 +0200

    Add Delete-Button

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,13 @@ import { useState } from "react";
 function App() {
   const [colors, setColors] = useState(initialColors);
   console.log("Submit wurde ausgeführt:", colors);
+
   function handleColorSubmit(newColor) {
     setColors((prevColors) => [newColor, ...prevColors]);
+  }
+
+  function handleDeleteColor(id) {
+    setColors((prevColors) => prevColors.filter((color) => color.id !== id));
   }
 
   return (
@@ -16,7 +21,13 @@ function App() {
       <h1>Theme Creator</h1>
       <ColorForm onSubmitColor={handleColorSubmit} />
       {colors.map((color) => {
-        return <Color key={color.id} color={color} />;
+        return (
+          <Color
+            key={color.id}
+            color={color}
+            onDeleteColor={handleDeleteColor}
+          />
+        );
       })}
     </>
   );
